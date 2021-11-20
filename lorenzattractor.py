01edb83091d5bda18c394667f123bb848bc2c9ed
Sam Tygier <sam.tygier@stfc.ac.uk>
2021-11-20 18:01:41 +0000

    Fix formula

--- lorenzattractor.py
+++ lorenzattractor.py
@@ -11,9 +11,9 @@ for i in range(10000):
     x, y, z = xs[i], ys[i], zs[i]
 
     # caclulate new
-    xs.append(x + (10*(y - x) * 0.01))
-    ys.append(y + (28*x - y - x*z * 0.01))
-    zs.append(z + (x*y - 2.667*z * 0.01))
+    xs.append(x + ((10*(y - x)) * 0.01))
+    ys.append(y + ((28*x - y - x*z) * 0.01))
+    zs.append(z + ((x*y - 2.667*z) * 0.01))
 
 ax = plt.figure().add_subplot(projection='3d')
 ax.plot(xs, ys, zs, lw=0.5)
