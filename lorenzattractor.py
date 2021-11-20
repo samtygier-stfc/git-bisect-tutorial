f17d5e44aeafdb1174e075921c30a5338d0ec284
Sam Tygier <sam.tygier@stfc.ac.uk>
2021-11-20 18:49:09 +0000

    Move variables to top of file

--- lorenzattractor.py
+++ lorenzattractor.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 import matplotlib.pyplot as plt
 
+nsteps = 10000
+dt = 0.01
+
 # Initial values
 xs = [0]
 ys = [1]
 zs = [1.05]
 
-nsteps = 10000
 for i in range(nsteps):
     # get current
     x, y, z = xs[i], ys[i], zs[i]
 
     # caclulate new
-    dt = 0.01
     xs.append(x + ((10*(y - x)) * dt))
     ys.append(y + ((28*x - y - x*z) * dt))
     zs.append(z + ((x*y - 2.667*z) * dt))
