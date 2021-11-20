be7320cca1586184d3066f6fa1487af69d707e5d
Sam Tygier <sam.tygier@stfc.ac.uk>
2021-11-20 18:01:00 +0000

    Plot lorenz attractor

--- lorenzattractor.py
+++ lorenzattractor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import matplotlib.pyplot as plt
 
 # Initial values
 xs = [0]
@@ -14,3 +14,7 @@ for i in range(10000):
     xs.append(x + (10*(y - x) * 0.01))
     ys.append(y + (28*x - y - x*z * 0.01))
     zs.append(z + (x*y - 2.667*z * 0.01))
+
+ax = plt.figure().add_subplot(projection='3d')
+ax.plot(xs, ys, zs, lw=0.5)
+plt.show()
