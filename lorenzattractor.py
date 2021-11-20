eaf80423a0eb6c59763b5bc204a006c28c561885
Sam Tygier <sam.tygier@stfc.ac.uk>
2021-11-20 17:59:04 +0000

    Calculate lorenz attractor

--- lorenzattractor.py
+++ lorenzattractor.py
@@ -5,3 +5,12 @@
 xs = [0]
 ys = [1]
 zs = [1.05]
+
+for i in range(10000):
+    # get current
+    x, y, z = xs[i], ys[i], zs[i]
+
+    # caclulate new
+    xs.append(x + (10*(y - x) * 0.01))
+    ys.append(y + (28*x - y - x*z * 0.01))
+    zs.append(z + (x*y - 2.667*z * 0.01))
